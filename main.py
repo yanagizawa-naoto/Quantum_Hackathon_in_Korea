ec28878471ec9134573fcb185d028c278e6f828e
Yunseong <dev.yunseong@gmail.com>
2026-02-25 20:36:14 +0900

    feat: add optimization router and update root message

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
+from router.optimization_router import router as optimization_router
 
 app = FastAPI()
 
+app.include_router(optimization_router)
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Quantum Hackathon API"}
 
 if '__main__' == __name__:
     uvicorn.run(app, host='0.0.0.0', port=8000)
